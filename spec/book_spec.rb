f6b8ffc97b55f495a3a351cbd37ecd8afe6cfe0d
J.J <johnidt@gmail.com>
2014-08-11 23:18:27 -0700

    test Vhs list method for books

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe 'Book' do
-  it 'is initialized with a name' do
-    test_book = Vhs::Book.new('name' => 'slaughterhouse 5')
-    expect(test_book.name).to eq 'slaughterhouse 5'
+  it 'is initialized with a title' do
+    test_book = Vhs::Book.new('title' => 'slaughterhouse 5')
+    expect(test_book.title).to eq 'slaughterhouse 5'
   end
 
   it 'can be saved into the database' do
@@ -16,5 +16,15 @@ describe 'Book' do
     test_book = Vhs::Book.new('title' => 'slaughterhouse 5')
     test_book_id = test_book.create
     expect(Vhs::Book.list.first.id.to_i).to eq test_book_id
+  end
+
+  describe '.list' do
+    it 'lists all books in the database' do
+      test_book = Vhs::Book.new('title' => 'slaughterhouse 5')
+      test_book2 = Vhs::Book.new('title' => 'galapagos')
+      test_book.create
+      test_book2.create
+      expect(Vhs::Book.list.length).to eq 2
+    end
   end
 end
