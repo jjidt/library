d32304f8bb993c583bb7d42b9834dce10460d623
J.J <johnidt@gmail.com>
2014-08-11 23:14:32 -0700

    test Vhs save for books

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -5,3 +5,16 @@ describe 'Book' do
     test_book = Vhs::Book.new('name' => 'slaughterhouse 5')
     expect(test_book.name).to eq 'slaughterhouse 5'
   end
+
+  it 'can be saved into the database' do
+    test_book = Vhs::Book.new('title' => 'slaughterhouse 5')
+    test_book.create
+    expect(Vhs::Book.list.first.title).to eq 'slaughterhouse 5'
+  end
+
+  it 'returns an id when saved into the database' do
+    test_book = Vhs::Book.new('title' => 'slaughterhouse 5')
+    test_book_id = test_book.create
+    expect(Vhs::Book.list.first.id.to_i).to eq test_book_id
+  end
+end
